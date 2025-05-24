1968615e1c41d21d7690fdb98455d1cd07986404
jatin-90920 <goyaljatin208@gmail.com>
2025-05-24 11:09:26 +0530

    cors allow

--- apps/package-service/src/index.ts
+++ apps/package-service/src/index.ts
@@ -15,12 +15,27 @@ const PORT = process.env.PORT || "5004";
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+
 app.use(
   cors({
-    origin: config.get<string>("origin"),
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
+// app.use(
+//   cors({
+//     origin: config.get<string>("origin"),
+//     credentials: true,
+//   })
+// );
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
